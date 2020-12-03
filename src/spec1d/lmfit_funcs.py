71dcc17a715549e11f3b1998d69c6fa03d8e38b9
sam <samgill844@gmail.com>
2020-12-03 16:03:18 +0000

    CR rejection and tracing

--- src/spec1d/lmfit_funcs.py
+++ src/spec1d/lmfit_funcs.py
@@ -3,12 +3,14 @@ import lmfit
 from lmfit.models import GaussianModel, PolynomialModel, VoigtModel,LorentzianModel, ExpressionModel , StepModel
 import numpy as np 
 import matplotlib.pyplot as plt 
+from scipy.ndimage import minimum_filter, median_filter
 
 #def box_lmfit(pars, x, p):
 #    height, center, width = p
 #    return height*(center-width/2 < x)*(x < center+width/2)
 
 box_model = ExpressionModel('height*(center-width/2 < x)*(x < center+width/2) + off')
+
 
 
 def fit_box_to_step_function(y,initial_guess, x = None, ):
@@ -40,6 +42,54 @@ def fit_box_to_step_function(y,initial_guess, x = None, ):
     plt.show(block=True)
     '''
     return  float(out.params['height'].value), float(out.params['center'].value), float(out.params['width'].value), float(out.params['off'].value), left, right
+
+
+
+
+def fit_box_to_step_function_science(y,initial_guess, x = None, ):
+    
+    # Create the x axis if needed
+    if x is None : x = np.arange(y.shape[0])
+
+    x = x[40:90]
+    y = y[40:90]
+
+    trend = minimum_filter(median_filter(y, 5) , 3)
+    y = y - trend
+
+    height = np.max(y)
+    off = 0.
+
+    #OLD WAY
+    # Create the parameters
+    params = Parameters()
+    params.add('height', value=height, min=0)
+
+    params.add('center', value=initial_guess[1], min=60, max = 80)
+    params.add('width', value=initial_guess[2], min=1e-3, max = 0.5*(np.max(x) - np.min(x)) )
+
+
+    params.add('off', value=off, min=np.min(y), max = 0.4*np.max(y) )
+
+
+    # now fit
+    out = box_model.fit(y, params, x=x)
+
+    left = float(out.params['center'].value) - 0.5*float(out.params['width'].value)
+    right = float(out.params['center'].value) + 0.5*float(out.params['width'].value)
+
+
+    '''
+    plt.figure()
+    plt.plot(x,y)
+    plt.plot(x, out.init_fit, 'k--', label='initial fit')
+    plt.plot(x, out.best_fit, 'r-', label='best fit')
+    plt.legend()
+    plt.show(block=True)
+    '''
+    return  float(out.params['height'].value), float(out.params['center'].value), float(out.params['width'].value), float(out.params['off'].value), left, right
+
+
 
     
 
